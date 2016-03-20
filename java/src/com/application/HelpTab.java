64d60fc32cfdaf69792ecab2d5c639f8a23114aa
CheokHo Mai <cheokho@hotmail.co.uk>
2016-03-20 18:25:30 +0000

    NDPDA new idea revamp testing. help guides added.

--- java/src/com/application/HelpTab.java
+++ java/src/com/application/HelpTab.java
@@ -15,10 +15,12 @@ public class HelpTab extends JDialog {
 
     public HelpTab() {
         setTitle("User Guide");
+        setIconImage(new ImageIcon(getClass().getResource("/com/icon/favicon.png")).getImage());
         createTabbedPane();
 
         setMinimumSize(new Dimension(700, 700));
         setLocationRelativeTo(null);
+        setResizable(false);
         setVisible(true);
 
     }
@@ -26,27 +28,33 @@ public class HelpTab extends JDialog {
     public void createTabbedPane() {
         JTabbedPane tabbedPane = new JTabbedPane();
         getContentPane().add(tabbedPane);
-        //ImageIcon image1 = new ImageIcon("/com/helpimages/Screen 1.png");
 
         try {
             BufferedImage image1 = ImageIO.read(HelpTab.class.getClassLoader().getResource("com/helpimages/screen1.png"));
             BufferedImage image2 = ImageIO.read(HelpTab.class.getClassLoader().getResource("com/helpimages/screen2.png"));
+            BufferedImage image3 = ImageIO.read(HelpTab.class.getClassLoader().getResource("com/helpimages/screen3.png"));
+            BufferedImage image4 = ImageIO.read(HelpTab.class.getClassLoader().getResource("com/helpimages/screen4.png"));
+            BufferedImage image5 = ImageIO.read(HelpTab.class.getClassLoader().getResource("com/helpimages/screen5.png"));
             JPanelMod panel1 = new JPanelMod();
             JPanelMod panel2 = new JPanelMod();
+            JPanelMod panel3 = new JPanelMod();
+            JPanelMod panel4 = new JPanelMod();
+            JPanelMod panel5 = new JPanelMod();
             panel1.setBackground(image1);
             panel2.setBackground(image2);
+            panel3.setBackground(image3);
+            panel4.setBackground(image4);
+            panel5.setBackground(image5);
 
             tabbedPane.addTab("Creating a new PDA", panel1);
             tabbedPane.addTab("Editing a PDA", panel2);
+            tabbedPane.addTab("Running a simulation", panel3);
+            tabbedPane.addTab("Saving and loading a PDA", panel4);
+            tabbedPane.addTab("Notation", panel5);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-//        tabbedPane.addTab("Editing a PDA", panel2);
-//        tabbedPane.addTab("Running a simulation", panel3);
-//        tabbedPane.addTab("Saving and loading a PDA", panel4);
-//        tabbedPane.addTab("Notation", panel5);
     }
 
 
